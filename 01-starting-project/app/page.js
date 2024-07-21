475cfce2f7a2cd7d544bd6ad29a92bc69be539f4
Kykke <kkomulainen@gmail.com>
2024-07-21 13:44:47 +0300

    Add some routes

--- 01-starting-project/app/page.js
+++ 01-starting-project/app/page.js
@@ -1,9 +1,17 @@
+import Link from "next/link";
+import HeaderComponent from "@/components/header";
+
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <HeaderComponent />
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p>
+        <Link href="/about">About Us</Link>
+        <p>
+          <Link href="/blog">Blog</Link>
+        </p>
+      </p>
     </main>
   );
 }
