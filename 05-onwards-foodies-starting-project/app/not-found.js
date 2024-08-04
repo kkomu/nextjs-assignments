29d17523bbd12a2fe1f800437f692fb718d496d8
Kykke <kkomulainen@gmail.com>
2024-08-04 22:08:17 +0300

    Add example Not Found page

---
+++ 05-onwards-foodies-starting-project/app/not-found.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+    return (
+        <main class="not-found">
+            <h1>Not found</h1>
+            <p>Unfortunately we couldn't find the requested page or resource</p>
+        </main>
+    );
+};
+
+export default NotFound;
