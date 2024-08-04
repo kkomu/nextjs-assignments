47e1d548e2c3c9b2024e9114d658f88701c47ca1
Kykke <kkomulainen@gmail.com>
2024-08-04 21:33:13 +0300

    Get meals from db and show them in a list

--- 05-onwards-foodies-starting-project/app/meals/page.jsx
+++ 05-onwards-foodies-starting-project/app/meals/page.jsx
@@ -1,11 +1,33 @@
-import Link from "next/link";
+import Link from 'next/link';
+import styles from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-const MealsPage = () => {
-  return (
-    <main>
-      <h1>Meals</h1>
-    </main>
-  );
+const MealsPage = async () => {
+    const meals = await getMeals();
+
+    return (
+        <>
+            <header className={styles.header}>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={styles.highlight}>by you</span>
+                </h1>
+                <p>
+                    Choose your favourite receipe and cook it yourself, It's
+                    easy and fun!
+                </p>
+                <p className={styles.cta}>
+                    <Link href="/meals/share">
+                        Share your favourite receipe
+                    </Link>
+                </p>
+            </header>
+            <main className={styles.main}>
+                <MealsGrid meals={meals} />
+            </main>
+        </>
+    );
 };
 
 export default MealsPage;
